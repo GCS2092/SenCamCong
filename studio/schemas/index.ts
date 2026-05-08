45b2adbe6960e120ac870944404787cf190cb8fa
GCS2092 <slovengama05@gmail.com>
2026-05-08 01:29:29 +0000

    fix: register new schemas in Sanity studio index

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,5 +1,9 @@
 import concert from './concert'
 import membre from './membre'
 import galerie from './galerie'
+import notreMusique from './notreMusique'
+import nosPerformances from './nosPerformances'
+import notreHistoire from './notreHistoire'
+import aPropos from './aPropos'
 
-export const schemaTypes = [concert, membre, galerie]
+export const schemaTypes = [concert, membre, galerie, notreMusique, nosPerformances, notreHistoire, aPropos]
